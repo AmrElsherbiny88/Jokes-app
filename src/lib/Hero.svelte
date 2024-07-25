2a87aa93565d74325abb78dfb84e2b3ea0ffe7a0
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-07-25 20:30:00 +0300

    Update Hero.svelte

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,17 +1,9 @@
 <script>
-
   import { goto } from "$app/navigation";
-
 </script>
-
-
-
-
 <h1 class='h1 mt-5'>Did your manager make you feel bad today?</h1>
-
 <button 
 class='variant-filled-error mt-5 rounded-md'
-on:click={()=> goto("/Home")} 
->
-    <h3 class='h3 p-3 '>So click here to get a joke</h3>
+onclick={()=> goto("/Home")} 
+>    <h3 class='h3 p-3 '>So click here to get a joke</h3>
 </button>
