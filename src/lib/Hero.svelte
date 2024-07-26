41c80663c12ecb357abd6a67d665837dff3c9e38
AmrElsherbiny <amoryelsherbiny@gmail.com>
2024-07-26 15:35:11 +0300

    .

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,9 +1,13 @@
 <script>
   import { goto } from "$app/navigation";
 </script>
-<h1 class='h1 mt-5'>Did your manager make you feel bad today?</h1>
-<button 
-class='variant-filled-error mt-5 rounded-md'
-onclick={()=> goto("/Home")} 
->    <h3 class='h3 p-3 '>So click here to get a joke</h3>
-</button>
+<div class="text-center">
+  <h1 class='h1 mt-5'>Did your manager make you feel bad today?</h1>
+  <button 
+  class='variant-filled-error mt-5 rounded-md hero text-center'
+  onclick={()=> goto("/Home")} 
+  >    <h3 class='h3 p-3 '>So click here to get a joke</h3>
+  </button>
+  
+  
+</div>
---
+++ src/routes/Home/+page.server.js
@@ -0,0 +1,17 @@
+export const load = async () => ({
+    joke: await fetch("https://v2.jokeapi.dev/joke/Any")
+      .then((response) => response.json())
+      .catch((error) => console.log(error)),
+  });
+  export const actions = {
+    getjoke: async () => await load(),
+    getcat: async ({ request }) => {
+      const { categ } = Object.fromEntries(await request.formData());
+      return {
+        joke: await fetch(`https://v2.jokeapi.dev/joke/${categ}`)
+          .then((response) => response.json())
+          .catch((error) => console.log(error)),
+      };
+    },
+  };
+  
